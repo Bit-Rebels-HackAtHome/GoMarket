1565a6f377818141c5e2d4bbf1ab9379386885f0
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2020-03-28 14:55:17 +0000

    migliorata entra.vue

--- frontend/src/pages/Registra.vue
+++ frontend/src/pages/Registra.vue
@@ -7,24 +7,63 @@
     >
     -->
 
-    <q-form>
+    <q-form ref="loginForm" @submit="onSubmit" class="g-gutter-sm">
       <q-input
         filled
         v-model="email"
         label="Il tuo indirizzo e-mail"
         hint="pippo@pluto.it"
         lazy-rules
+        v-bind:rules="Richiesto"
       />
 
-      <q-input filled v-model="password" label="La tua password" />
+      <q-input filled required v-model="password" type="password" label="La tua password" />
 
-      <q-input filled v-model="password_ripetuta" label="Inserisci di nuovo la tua password" />
+      <q-input
+        filled
+        v-model="password_ripetuta"
+        type="password"
+        required
+        label="Inserisci di nuovo la tua password"
+        v-bind:rules="Richiesto"
+      />
+
+      <q-input filled v-model="lat_residenza" type="number" v-bind:rules="Richiesto" />
+
+      <q-input filled v-model="long_residenza" type="number" v-bind:rules="Richiesto" />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary" />
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
     </q-form>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: "PageIndex"
+  name: "Registra",
+
+  data() {
+    return {
+      email: "",
+      password: "",
+      password_ripetuta: "",
+      lat_residenza: 0.0,
+      long_residenza: 0.0
+    };
+  },
+
+  methods: {
+    onSubmit: function(event) {
+      console.log(event);
+    }
+  },
+
+  computed: {
+    Richiesto() {
+      return [v => !!v || "Il campo non può essere vuoto."];
+    }
+  }
 };
 </script>
