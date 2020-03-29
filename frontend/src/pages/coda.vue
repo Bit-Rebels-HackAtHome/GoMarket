5283868b5bb732a5f3d5e2f0b8a20f04a59e4a85
Your Name <you@example.com>
2020-03-29 11:32:11 +0200

    fix

--- frontend/src/pages/coda.vue
+++ frontend/src/pages/coda.vue
@@ -1,18 +1,24 @@
 <template>
   <q-page class="column flex-center justify-evenly">
-      <q-card class="column">
-          <q-chip icon="shop">{{name}}</q-chip>
-          <q-chip icon="map">{{address}}</q-chip>
-      </q-card>
+      <div class="row q-gutter-sm flex-center q-py-sm">
+          <div icon="shop" class="finta-chip q-px-sm">
+            <span>{{name}}</span>
+            </div>
+          <div icon="map" class="finta-chip q-px-sm">
+            <span>{{address}}</span>
+          </div>
+      </div>
 
-      <q-list>
+      <q-list class="row flex-center">
         <q-chip
+        class="col-11"
         v-for="h in 24"
         :key="h+0.1"
         clickable
         v-ripple
         square
-        :color="(h>7 && h<20)? 'secondary':'red'"
+        v-show="(h>7 && h<19)"
+        color="blue"
         text-color="white"
         >
                 ore : {{h}}:00
@@ -35,3 +41,15 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+
+.finta-chip{
+  background-color: #26a69a;
+  color:rgb(247, 247, 247);
+  width:90vw;
+  border-radius: 4px;
+  box-shadow:2px 2px 3px #ccc;
+}
+
+</style>
